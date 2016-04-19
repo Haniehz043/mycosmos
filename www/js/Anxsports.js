c8621e31415ba02a1973efa5e0f2b033e22eccb9
Haniehz043 <haniehz043@outlook.com>
2016-04-18 23:22:25 -0700

    Update Anxsports.js

--- www/js/Anxsports.js
+++ www/js/Anxsports.js
@@ -4,7 +4,7 @@ angular.module('starter')
  $scope.sports =  [
 
    {imageSrc: '../img/Angry/Angrysports/BS.png', url: 'https://www.youtube.com/watch?v=UgzXRXmAOMA'},
-   {imageSrc: '../img/Angry/Angrysports/Hock.png', url: 'https://www.youtube.com/watch?v=UgzXRXmAOMA'},
+   {imageSrc: '../img/Angry/Angrysports/Hock.png', url: 'https://www.youtube.com/watch?v=vdiLp5pnr_k'},
    {imageSrc: '../img/Angry/Angrysports/kickboxing.jpg', url: 'https://www.youtube.com/watch?v=RVr9mVNdQc4'},
    {imageSrc: '../img/Angry/Angrysports/man.png', url: 'https://www.youtube.com/watch?v=AWS_zIFfGTg'},
    {imageSrc: '../img/Angry/Angrysports/woman-running1.svg', url: 'https://www.youtube.com/watch?v=e7m205ZIxBE'},
